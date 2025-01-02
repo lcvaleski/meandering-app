2e010bc8d170e7659f42f3d9df56469dcf5d1bf4
Jud Valeski <jvaleski@gmail.com>
2025-01-02 15:50:19 -0700

    removing email saving as we're no longer using it.

--- lib/utils.dart
+++ lib/utils.dart
@@ -5,11 +5,3 @@ String getDayOfWeekString(DateTime now) {
   final List<String> days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   return days[dayOfWeek - 1];  // Subtract 1 to match the index
 }
-
-Future<http.Response> saveEmail(String email) async {
-  final url = Uri.https('coventrylabs.net', '/email-subscribe');
-  final headers = <String, String> {'Content-Type': 'application/json'};
-  return await http.post(url,
-      headers: headers,
-      body: '{"email": "$email"}');
-}
